db2b3a2158c923b0fa4698a86744d5bfd70952f5
JamuLeo <bsc-com-06-21@unima.ac.mw>
2025-06-29 13:03:27 +0100

    UI successfully connected with backend and works perfectly

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import Landingpage from './pages/Landingpage'
+import Dashboard from './pages/Home/Dashboard'
+import InterviewPrep from './pages/interviewPrep/InterviewPrep'
+import UserProvider from './context/useContext'
 
-export default function App() {
+function App() {
   return (
-	<div className="bg-red-500 text-2xl">App</div>
+    <UserProvider>
+      <div className=''>
+        <Router>
+          <Routes>
+            {/* Default Route */}
+            <Route path='/' element={<Landingpage />} />
+
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/interview-prep/:sessionId' element={<InterviewPrep />} />
+          </Routes>
+        </Router>
+        <Toaster
+          toastOptions={{
+            style: {
+              fontSize: "13px",
+            },
+          }}
+        />
+      </div>
+    </UserProvider>
+
   )
 }
+
+export default App
